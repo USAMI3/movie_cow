4d93ab2d0beadb9db39cf92e5a59c5c33e626b27
USAMI3 <usama.personal3@gmail.com>
2023-02-20 17:30:05 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ystop_mystop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.movie_cow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:movie_cow/routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialize();
+  bool isNetwork = await isNetworkAvailable();
+  print(isNetwork);
+  runApp(const ProviderScope(
+    child: App(),
+  ));
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample Project',
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      // Update app theme data here
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: Routes.routes,
+      initialRoute: '/login',
+    );
+  }
+}
